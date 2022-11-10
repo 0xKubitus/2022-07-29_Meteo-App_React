22b8edd688ac7a87057a8f064bfabe131d73b2da
0xKubitus <n.cubilier@gmail.com>
2022-11-10 14:30:06 +0100

    project well initialized, go work using branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-// import
+import { useState, useEffect } from "react";
+
+const openWeatherAPIurlStart = "https://api.openweathermap.org/data/2.5/forecast?";
 
 function App() {
+    const [lat, setLat] = useState(0);
+    const [lon, setLon] = useState(0);
+
     return <div className="App"></div>;
 }
 
