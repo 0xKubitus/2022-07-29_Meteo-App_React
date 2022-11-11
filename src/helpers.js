a4929d7babf96c07e6d0e5719378f65166c74152
0xKubitus <n.cubilier@gmail.com>
2022-11-11 02:37:30 +0100

    quick refacto

---
+++ src/helpers.js
@@ -0,0 +1,24 @@
+// import { useState, useEffect } from "react";
+
+export const getDate = (date) => {
+    const d = new Date(date * 1000);
+    const day = d.getDay();
+    const getDay = () => {
+        if (day === 1) {
+            return "Monday";
+        } else if (day === 2) {
+            return "Tuesday";
+        } else if (day === 3) {
+            return "Wednesday";
+        } else if (day === 4) {
+            return "Thursday";
+        } else if (day === 5) {
+            return "Friday";
+        } else if (day === 6) {
+            return "Saturday";
+        } else {
+            return "Sunday";
+        }
+    };
+    return new Date().getDay() === day ? "Today" : getDay();
+};
