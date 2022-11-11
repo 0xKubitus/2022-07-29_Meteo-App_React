c17dbaa0b648d4a30bd4f085ffb8e16bb81cb93d
0xKubitus <n.cubilier@gmail.com>
2022-11-11 14:35:19 +0100

    new way of generating cards works well, I now need to make a function that finds the min & max Temp within each day, etc but I'd like to try with another API request because it might be easier to use if I don't have to build my own functions that calculate min & max temp within a day...

--- src/components/CardTemplate.jsx
+++ src/components/CardTemplate.jsx
@@ -1,21 +1,41 @@
 import { getDate } from "helpers.js";
 
 const CardTemplate = ({ data }) => {
-    const date = getDate(data.dt);
+    const date = getDate(data[0].dt);
 
     return (
         <div>
-            {data !== "undefined" && (
+            {data[0] !== "undefined" && (
                 <div>
                     <p>{date}</p>
-                    <p>{data.dt_txt} forecast</p>
+                    <p>{data[0].dt_txt}</p>
                 </div>
             )}
-            <p>{data.weather[0].description}</p>
+            <p>{data[0].weather[0].description}</p>
             <p>
-                Min. = {data.main.temp_min}°F / Max = {data.main.temp_max}°F
+                Min. = {data[0].main.temp_min}°F / Max = {data[0].main.temp_max}°F
             </p>
         </div>
     );
 };
 export default CardTemplate;
+
+// const CardTemplate = ({ data }) => {
+//     const date = getDate(data.dt);
+
+//     return (
+//         <div>
+//             {data !== "undefined" && (
+//                 <div>
+//                     <p>{date}</p>
+//                     <p>{data.dt_txt} forecast</p>
+//                 </div>
+//             )}
+//             <p>{data.weather[0].description}</p>
+//             <p>
+//                 Min. = {data.main.temp_min}°F / Max = {data.main.temp_max}°F
+//             </p>
+//         </div>
+//     );
+// };
+// export default CardTemplate;
