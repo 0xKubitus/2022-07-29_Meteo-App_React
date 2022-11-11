af380ee85de51ee017dabda9e8cda36e118fe240
0xKubitus <n.cubilier@gmail.com>
2022-11-11 17:00:11 +0100

    Cards need Icons

--- src/components/CardTemplate.jsx
+++ src/components/CardTemplate.jsx
@@ -1,4 +1,4 @@
-import { getDate } from "helpers.js";
+import { getDate, convertKelvinToFarenheit } from "helpers.js";
 
 const CardTemplate = ({ data }) => {
     const date = getDate(data[0].dt);
@@ -12,36 +12,44 @@ const CardTemplate = ({ data }) => {
         maxTemps.push(entry.main.temp_max);
         minTemps.push(entry.main.temp_min);
     });
-    console.log(`${date} max temps =`, maxTemps);
-    console.log(`${date} min temps =`, minTemps);
+    // console.log(`${date} max temps =`, maxTemps);
+    // console.log(`${date} min temps =`, minTemps);
 
     const highestKelvinTemp = Math.max(...maxTemps);
     const lowestKelvinTemp = Math.min(...minTemps);
-    console.log(`${date} highestKelvinTemp =`, highestKelvinTemp);
-    console.log(`${date} lowestKelvinTemp =`, lowestKelvinTemp);
+    // console.log(`${date} highestKelvinTemp =`, highestKelvinTemp);
+    // console.log(`${date} lowestKelvinTemp =`, lowestKelvinTemp);
 
-    const convertKelvinToFarenheit = (temp) => {
-        const tempInFarenheit = (parseFloat(temp) - 273.15) * 1.8 + 32;
-        // console.log(temp, tempInFarenheit);
-        return tempInFarenheit.toFixed(2);
-    };
-    convertKelvinToFarenheit(highestKelvinTemp);
-    convertKelvinToFarenheit(lowestKelvinTemp);
+    // const convertKelvinToFarenheit = (temp) => {
+    //     const tempInFarenheit = (parseFloat(temp) - 273.15) * 1.8 + 32;
+    //     // console.log(temp, tempInFarenheit);
+    //     return tempInFarenheit.toFixed(2);
+    // };
+    // convertKelvinToFarenheit(highestKelvinTemp);
+    // convertKelvinToFarenheit(lowestKelvinTemp);
 
     return (
         <div className="card-template">
             {data[0] !== "undefined" && (
                 <div>
-                    <p>{date}</p>
-                    <p>{data[0].dt_txt}</p>
+                    <p>
+                        <b>{date}</b>
+                        <br></br>
+                        {data[0].dt_txt}
+                    </p>
                 </div>
             )}
             <p>
-                {data[0].weather[0].description} - {data[0].main.temp}°F
+                <b>
+                    {data[0].weather[0].description}
+                    <br></br>
+                    {convertKelvinToFarenheit(data[0].main.temp)}°F
+                </b>
             </p>
             <p>
                 {/* Daily Min. = {data[0].main.temp_min}°F */}
-                Daily Min. = {lowestKelvinTemp}°F
+                {/* Daily Min. = {lowestKelvinTemp}°F */}
+                Daily Min. = {convertKelvinToFarenheit(lowestKelvinTemp)}°F
                 <br></br>
                 {/* Daily Max = {data[0].main.temp_max}°F */}
                 Daily Max = {convertKelvinToFarenheit(highestKelvinTemp)}°F
